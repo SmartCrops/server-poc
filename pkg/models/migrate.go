1092aecb7be1563fa9286b5b22f2745b7b89dd47
Kuba Smolaga <smolagakuba@gmail.com>
2022-12-07 23:57:17 +0100

    added model migrations

--- pkg/models/migrate.go
+++ pkg/models/migrate.go
@@ -5,5 +5,10 @@ import "gorm.io/gorm"
 func MigrateAll(db *gorm.DB) error {
 	return db.AutoMigrate(
 		&SensorData{},
+		&DataCollector{},
+		&Installation{},
+		&Pump{},
+		&PumpController{},
+		&User{},
 	)
 }
